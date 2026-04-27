fff8321f5e79dc16d80f7909013a2dffeebc21b6
msr275 <1251275@isep.ipp.pt>
2026-04-27 13:07:15 +0100

    -Edit of main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,3 +1,27 @@
-//
-// Created by mateu on 27/04/2026.
-//
+#include <iostream>
+#include <string>
+//#include <Utils.h>
+
+void displayHeader()
+{
+    std::cout <<"Music Store of Group 3"<< std::endl;
+
+}
+
+int main()
+{
+
+    int choice = -1;
+
+    while ( choice != 0 )
+    {
+        displayHeader();
+        std::cout <<"1. Browse Catalog"<< std::endl;
+        std::cout <<"Music Store of Group 3"<< std::endl;
+        std::cout <<"Music Store of Group 3"<< std::endl;
+        std::cout <<"Music Store of Group 3"<< std::endl;
+
+    }
+
+
+}
