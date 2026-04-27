760d63120bf9116a9f02e805e55b874fe7d356fb
msr275 <1251275@isep.ipp.pt>
2026-04-27 18:28:48 +0100

    -main.cpp updated

--- Project/main.cpp
+++ Project/main.cpp
@@ -17,11 +17,30 @@ int main()
     {
         displayHeader();
         std::cout <<"1. Browse Catalog"<< std::endl;
-        std::cout <<"Music Store of Group 3"<< std::endl;
-        std::cout <<"Music Store of Group 3"<< std::endl;
-        std::cout <<"Music Store of Group 3"<< std::endl;
+        std::cout <<"2. My Shopping Cart"<< std::endl;
+        std::cout <<"3. Admin Login"<< std::endl;
+        std::cout <<"0. Exit"<< std::endl;
+        std::cout <<"\nYour Choice: ";
 
+
+        if (!(std::cin >> choice))
+        {
+            std::cout <<"\nInvalid Choice!"<< std::endl;
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            choice = -1;
+            continue;
+        }
+
+
+        switch (choice)
+        {
+            case 1: std::cout <<"Opening Catalog."<< std::endl; break;
+            case 2: std::cout <<"Opening Cart."<< std::endl; break;
+            case 3: std::cout <<"Login Required."<< std::endl; break;
+            case 0: std::cout <<"See ya next time!"<< std::endl; break;
+            default: std::cout <<"That option does not exist."<< std::endl; break;
+        }
     }
-
-
+    return 0;
 }
