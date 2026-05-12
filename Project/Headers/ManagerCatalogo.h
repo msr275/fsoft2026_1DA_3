fabae412ea35a0b3e98ce274459702abcef869bc
Rodrigo Santos <1251419@isep.ipp.pt>
2026-05-12 16:56:10 +0100

    catalogo

--- Project/Headers/ManagerCatalogo.h
+++ Project/Headers/ManagerCatalogo.h
@@ -18,9 +18,15 @@ public:
     //adiciona aos vetores respetivos
     void adicionarArtista(const std :: string& nome,const std :: string& pais,const std :: string& genero);
     void adicionarAlbum(const Album &b);
-
+    //remove dos vetores
     bool removerAlbum(int idParaApagar);
     bool removerArtista(int idParaApagar);
+
+    //PESQUISA
+    std::vector<Album> pesquisarPorGenero(const std::string& generoProcurado);
+    std::vector<Album> pesquisaPorAno(int anoProcurado);
+    std::vector<Album> pesquisarPorNomedeAlbum(const std::string& nomeProcurado);
+    std::vector<Album> pesquisaPorNomedeArtista(const std::string& artistaProcurado);
 };
 
 #endif //FSOFT2026_1DA_3_MANAGERCATALOGO_H
--- Project/Sources/ManagerCatalogo.cpp
+++ Project/Sources/ManagerCatalogo.cpp
@@ -15,6 +15,8 @@ private:
     int idproximoAlbum = 1;
 
 public:
+
+    //--------------------------------------EDIÇÃO DE CATÁLOGO------------------------------
     //adiciona aos vetores respetivos
     void adicionarArtista(const std :: string& nome,const std :: string& pais,const std :: string& genero) {
         Artista a(idproximoArtista,nome,pais,genero);
@@ -26,8 +28,8 @@ public:
         bool artistaEncontrado = false;
 
         //verifica se exista já o artista
-        for (int z = 0; z < listaArtistas.size(); z++) {
-            if (listaArtistas.at(z).get_id_artista() == id_artista) {
+        for (int i = 0; i < listaArtistas.size(); i++) {
+            if (listaArtistas[i].get_id_artista() == id_artista) {
                 artistaEncontrado = true;
                 break;
             }
@@ -67,4 +69,73 @@ public:
         }
         return false;
     }
+
+
+
+
+   //---------------------------------- PESQUISA----------------------------------------
+
+//pesquisa por género, vai retornar vetor de albuns que correspondem a X género
+    std::vector<Album> pesquisarPorGenero(const std::string& generoProcurado) {
+        std::vector<Album> resultados;
+//percorre albuns, vai ver cada artista associado ao album e ve o seu género associado, se corresponder, adiciona ao vetor resultados
+        for (int i = 0; i < listaAlbuns.size(); i++) {
+            for (int j = 0; j < listaArtistas.size(); j++) {
+                if (listaArtistas[j].get_id_artista() == listaAlbuns[i].id_artista()) {
+                    if (listaArtistas[j].get_genero() == generoProcurado) {
+                        resultados.push_back(listaAlbuns[i]);
+                    }
+                    break;
+                }
+            }
+        }
+        return resultados;
+    }
+
+//pesquisa por ano
+    std::vector<Album> pesquisaPorAno(int anoProcurado) {
+        std::vector<Album> resultados;
+
+        for (int i=0; i< listaAlbuns.size();i++) {
+            if (listaAlbuns[i].ano1()==anoProcurado) {
+                resultados.push_back((listaAlbuns[i]));
+            }
+        }
+        return resultados;
+    }
+
+//pesquisa por nome de Álbum
+    std::vector<Album> pesquisarPorNomedeAlbum(const std::string& nomeProcurado) {
+        std::vector<Album> resultados;
+
+        for (int i = 0; i < listaAlbuns.size(); i++) {
+            std::string tituloAlbum = listaAlbuns[i].titulo1();
+            //find procura nomes semelhantes ao escrito pelo utilizador e devolve o sítio(indíce) em q a palavra começa
+            if (tituloAlbum.find(nomeProcurado) != std::string::npos) { //npos basicamente diz q n encontrou nada
+                resultados.push_back(listaAlbuns[i]);
+            }
+        }
+        return resultados;
+    }
+
+    // pesquisa por nome de artista
+    std::vector<Album> pesquisaPorNomedeArtista(const std::string& artistaProcurado) {
+        std::vector<Album> resultados;
+//percorre albuns e ve artista associado, guardando o seu nome. Usa-se se o find para ver se o utilizador pesquisou por algo parecido ocm esse artista
+        for (int i = 0; i < listaAlbuns.size(); i++) {
+            for (int j = 0; j < listaArtistas.size(); j++) {
+                if (listaArtistas[j].get_id_artista() == listaAlbuns[i].id_artista()) {
+                    std::string nomeArtista = listaArtistas[j].get_nome();
+
+                    if (nomeArtista.find(artistaProcurado) != std::string::npos) {
+                        resultados.push_back(listaAlbuns[i]);
+                    }
+                    break;
+                }
+            }
+        }
+        return resultados;
+    }
+
+    //FALTA PESQUISA COM TODOS CRITÉRIOS AO MSM TEMPO
 };
