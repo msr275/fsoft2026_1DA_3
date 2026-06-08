dba724b3d0c7ebe801d03915fef660d880c1efd4
msr275 <1251275@isep.ipp.pt>
2026-06-08 10:59:45 +0100

    -adição de código para formatação correta em win32

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,6 +1,15 @@
 #include "Headers/views/Menus.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main() {
+
+    #ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    #endif
+
     runMainMenu();
     return 0;
 }
