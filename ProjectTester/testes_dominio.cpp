6c0f74f327903d4106615af4135d8f822d0c068d
msr275 <1251275@isep.ipp.pt>
2026-06-09 20:31:38 +0100

    -modificação CMakeLists.txt -adição de testagens a testes_dominio.cpp

--- ProjectTester/testes_dominio.cpp
+++ ProjectTester/testes_dominio.cpp
@@ -2,10 +2,14 @@
 #include <models/Album.h>
 #include <models/Artista.h>
 #include <models/Carrinho.h>
+#include <models/Rating.h>
+#include <models/Venda.h>
+#include <persistence/DataManager.h>
+#include <controllers/Pesquisa.h>
 #include <exceptions/InvalidDataException.h>
 
 
-// Testes da Classe Artista
+// 1. TESTES UNITÁRIOS: Artista (Artista Válido, Id por Nome e Bloquear Campos Vazios)
 
 TEST(ArtistaTest, CriarArtistaValido) {
     EXPECT_NO_THROW({
@@ -24,7 +28,7 @@ TEST(ArtistaTest, BloquearCamposVazios) {
 
 
 
-// Testes da Classe Álbum
+// 2. TESTES UNITÁRIOS: Álbum (Preço, Formato e Anos de Fronteira)
 
 TEST(AlbumTest, ValidarPrecoNegativo) {
     Artista artistaFake(1, "Pink Floyd", "UK" , "Rock");
@@ -47,19 +51,45 @@ TEST(AlbumTest, ValidarFormatos) {
     }, InvalidDataException);
 }
 
+TEST(AlbumTest, ValidarLimitesDoAno) {
+    Artista artistaFake(1, "Pink Floyd", "UK" , "Rock");
+    Album albumTeste(1, &artistaFake, "Wish You Were Here", 1975 , 29.99f, "Vinil");
 
-// Testes da Classe Carrinho
+    EXPECT_THROW({
+        albumTeste.set_ano(2027);
+    },  InvalidDataException);
 
-TEST(CarrinhoTest, ValidarOperacoesECalculoTotal) {
+    EXPECT_THROW({
+        albumTeste.set_ano(1899);
+    },  InvalidDataException);
+}
+
+
+// 3. TEST FIXTURES (TEST_F): Reutilização e Gestão de Memória
+
+class CarrinhoFixture : public ::testing::Test {
+    protected:
+    Artista* artistaFake;
+    Album* alb1;
+    Album* alb2;
     Carrinho carrinho;
-    Artista artistaFake(1, "Pink Floyd", "UK", "Rock");
 
-    Album alb1(1, &artistaFake, "Wish You Were Here", 1975, 29.99f, "Vinil");
-    Album alb2(2, &artistaFake, "Dark Side of the Moon", 1975, 37.99f, "Vinil");
+    void SetUp() override {
+        artistaFake = new Artista(1, "Pink Floyd", "UK" , "Rock");
+        alb1 = new Album(1, artistaFake, "Wish You Were Here", 1975 , 29.99f, "Vinil");
+        alb2 = new Album(2, artistaFake, "The Dark Side of the Moon", 1973 , 37.99f, "Vinil");
+    }
 
-    carrinho.adicionarAlbum(&alb1);
-    carrinho.adicionarAlbum(&alb2);
+    void TearDown() override {
+        delete alb1;
+        delete alb2;
+        delete artistaFake;
+    }
+};
 
+TEST_F(CarrinhoFixture, ValidarOperacoesECalculoTotal) {
+    carrinho.adicionarAlbum(alb1);
+    carrinho.adicionarAlbum(alb2);
 
     EXPECT_FLOAT_EQ(carrinho.calcularTotal(), 67.98f);
     EXPECT_EQ(carrinho.getItens().size(), 2);
@@ -69,5 +99,43 @@ TEST(CarrinhoTest, ValidarOperacoesECalculoTotal) {
 
     carrinho.limparCarrinho();
     EXPECT_EQ(carrinho.getItens().size(), 0);
-    EXPECT_FLOAT_EQ(carrinho.calcularTotal(), 0.00f);
+}
+
+
+//  4. INTEGRAÇÃO: Persistência  (Ficheiros CSV)
+
+TEST(IntegracaoPersistenciaTest, CarregarDadosDoSistema) {
+
+    EXPECT_NO_THROW({
+        DataManager::getInstance().carregarDados();
+    });
+}
+
+
+//  5. INTEGRAÇÃO: Controller de Pesquisa
+
+TEST(IntegracaoPesquisaTest, MotoresDeBusca) {
+
+    EXPECT_NO_THROW({
+        controllerPesquisa.pesquisarPorGenero("Rock");
+        controllerPesquisa.pesquisaPorArtista("Kendrick Lamar");
+    });
+}
+
+
+//  6. INTEGRACAO: Regras de Negócio (Venda e Rating)
+
+TEST(IntegracaoRegrasNegocioTest, FluxosEfetivos) {
+
+    Artista artistaFake(1, "Pink Floyd", "UK" , "Rock");
+    Album albumTeste(1, &artistaFake, "Wish You Were Here", 1975 , 29.99f, "Vinil");
+
+    Rating ratingTeste;
+    EXPECT_NO_THROW({
+        ratingTeste.adicionarNota(5);
+    });
+
+    EXPECT_NO_THROW({
+        Venda novaVenda(1, "2026-06-09", 29.99f);
+    });
 }
